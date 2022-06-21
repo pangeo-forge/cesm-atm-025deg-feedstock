20ab126604bbb34b8c23f4f20ac861bb8364c5d3
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-06-21 15:23:03 -0700

    restore subset_inputs w/ time: 2

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -45,4 +45,5 @@ target_chunks = {"time": 30} # Full spatial domain: "lat": 768, "lon": 1152, -->
 recipe = XarrayZarrRecipe(
             file_pattern=pattern,
             target_chunks=target_chunks,
+            subset_inputs = {"time": 2},
             )
